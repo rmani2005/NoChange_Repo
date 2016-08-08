5958c5e39afba022ef559c6fc4b30dfcfa76e3be
mravi <mravi@SVWIN00058.harrisbroadcast.com>
2016-08-08 17:39:46 +0530

    j

--- Nochange/src/Test/LoginTest.java
+++ Nochange/src/Test/LoginTest.java
@@ -1,11 +1,23 @@
 package Test;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class LoginTest 
 {
+	WebDriver driver;
 
-	{
-		
+		@Test
+		public void LoginMethod()
+		{
+			driver=new FirefoxDriver();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.get("http://ischolar.rsstv.com");
+		}
 
-	}
+
 
 }
