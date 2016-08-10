200da74dd97374dd5161fdf211e97f614d22837a
mravi <mravi@SVWIN00058.harrisbroadcast.com>
2016-08-10 10:08:53 +0530

    Hmmmm

--- Nochange/src/Test/LoginTest.java
+++ Nochange/src/Test/LoginTest.java
@@ -4,20 +4,38 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class LoginTest 
 {
-	WebDriver driver;
+	public WebDriver driver;
+	
+	@BeforeTest
+	public void setup1()
+	{
+		driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://ischolar.rsstv.com");
+	}
 
 		@Test
-		public void LoginMethod()
+		public void loginMethod()
 		{
-			driver=new FirefoxDriver();
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			driver.get("http://ischolar.rsstv.com");
+			driver.manage().window().fullscreen();
+			System.out.println("Test111111111111111111111111");
 		}
+		
+		@Test
+		public void maxi()
+		{
+			driver.manage().window().maximize();
+			System.out.println("Test22222222222222222222222222");
+		}	
 
-
+		@AfterTest
+		public void finalset()
+		{
+			driver.quit();
+		}
 
 }
