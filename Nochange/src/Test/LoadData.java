32877743728305643f13dce4856d11a5b1e10b1a
Mani <Mani@DESKTOP-38OR8B2>
2016-08-10 22:25:48 +0530

    from home 10 aug

--- Nochange/src/Test/LoadData.java
+++ Nochange/src/Test/LoadData.java
@@ -3,6 +3,11 @@ package Test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
 
 public class LoadData {
 
@@ -11,14 +16,24 @@ public class LoadData {
 		
 	}
 
-	public void readExcel() throws FileNotFoundException
+	public void readExcel(String filename) throws IOException
 	
 	{
+		
+		
 		try{
 		File fi=new File("OR.PROPERTIES");
 		FileInputStream fis = new FileInputStream(fi);
-		
-		
+		Workbook wb=null;
+		String fileext=filename.substring(filename.indexOf("."));
+		if(fileext.equals(".xlsx"))
+		{
+			wb=new XSSFWorkbook(fis);
+		}
+		else if(fileext.equals(".xls"))
+		{
+			wb=new HSSFWorkbook(fis);
+		}
 		}catch(FileNotFoundException e)
 		{
 		System.out.println(e.getStackTrace());
@@ -27,4 +42,26 @@ public class LoadData {
 		
 	}
 
+	
+	public Sheet getShname(String filename, String Sheetname) throws IOException
+	{
+		
+			File fi=new File("OR.PROPERTIES");
+			FileInputStream fis = new FileInputStream(fi);
+			Workbook wb=null;
+			String fileext=filename.substring(filename.indexOf("."));
+			if(fileext.equals(".xlsx"))
+			{
+				wb=new XSSFWorkbook(fis);
+			}
+			else if(fileext.equals(".xls"))
+			{
+				wb=new HSSFWorkbook(fis);
+			}	
+			Sheet sh = wb.getSheet(Sheetname);
+			return sh;
+			
+	
+	}
+	
 }
