34197064719d662619a4e19b82f303f72a89637e
Mani <Mani@DESKTOP-38OR8B2>
2016-08-13 00:00:30 +0530

    Home 14/08/2016

--- Nochange/src/Test/DriverLibrary.java
+++ Nochange/src/Test/DriverLibrary.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 public class DriverLibrary {
 
@@ -14,21 +17,45 @@ public class DriverLibrary {
 		
 		if(browser.equals("firefox"))
 		{
-			System.setProperty("webdriver.firefox.bin", "C:\\Users\\319468\\");
+			System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
 			driver=new FirefoxDriver();			
 		}
 		else if(browser.equals("chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\319468\\");
+			System.setProperty("webdriver.chrome.driver", "D:\\Mani\\Selenium\\chromedriver.exe");
 			driver=new ChromeDriver();			
 		}
 		else if(browser.equals("ie"))
 		{
-			System.setProperty("webdriver.ie.driver", "C:\\Users\\319468\\");
+			System.setProperty("webdriver.ie.driver", "");
 			driver=new InternetExplorerDriver();			
 		}
+		else
+		{
+			System.out.println("Invalid Browser, Please choose the relevant browser");
+			System.exit(0);
+		}
+	}
+		@BeforeClass
+		public void preConfig()
+		{
+			ReadObjPro.loadpropertyfile();
+		}
+		
+		@BeforeMethod
+		public void setup()
+		{
+			loadDriver(ReadObjPro.getTestData("browser"));
+		}
+		
+		@AfterMethod		
+		public void terminate()
+		{
+			driver.quit();
+		}
+		
 		
 	}
 
 	
-}
+
--- Nochange/src/Test/ReadObjPro.java
+++ Nochange/src/Test/ReadObjPro.java
@@ -2,6 +2,7 @@ package Test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,16 +10,41 @@ import java.util.Properties;
 public class ReadObjPro 
 {
 
-	Properties oData= new Properties();
-	Properties aData= new Properties();
-	public Properties getOPro() throws IOException
+	static Properties oData= new Properties();
+	static Properties aData= new Properties();
+	
+	public static void loadpropertyfile()
+	{
+		try{
+		oData.load(new FileInputStream("objectRepository.properties"));
+		aData.load(new FileInputStream("AND.PROPERTIES"));
+		}catch(FileNotFoundException e){
+			System.out.println(e.getStackTrace());
+		}catch(IOException e){
+			System.out.println(e.getStackTrace());
+		}
+		
+	}
+		
+		public static String getTestData(String property)
+		{
+			return oData.getProperty(property);
+		}
+		
+
+		public static String getTestObj(String property)
+		{
+			return aData.getProperty(property);
+		}
+
+	public static Properties getOPro() throws IOException
 	{
 	InputStream stream=new FileInputStream(new File(System.getProperty("user.dir"))+ "objectRepository.properties");
 	oData.load(stream);
 	return oData;
 	}
 	
-	public Properties getAPro() throws IOException
+	public static Properties getAPro() throws IOException
 	{
 	InputStream stream=new FileInputStream(new File(System.getProperty("user.dir"))+ "AND.PROPERTIES");
 	aData.load(stream);
